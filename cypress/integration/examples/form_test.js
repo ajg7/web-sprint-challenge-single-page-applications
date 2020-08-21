c1f3b3ded7ca6b98def77e3334a8a804b05a015b
ajg7 <ajgebara@gmail.com>
2020-08-21 12:50:57 -0500

    fixed tests

--- cypress/integration/examples/form_test.js
+++ cypress/integration/examples/form_test.js
@@ -2,7 +2,7 @@
 
 describe('Team Builder Test', () => {
     it('can navigate to http://localhost:3000', () => {
-        cy.visit('http://localhost:3000')
+        cy.visit('http://localhost:3000/pizza')
         cy.url().should('include', 'localhost')
     })
     describe("Inputs Test", () => {
