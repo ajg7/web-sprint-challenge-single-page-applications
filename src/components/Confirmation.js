2c44378cf66d4521d36eac54088fe4e07c956a23
ajg7 <ajgebara@gmail.com>
2020-08-21 11:03:37 -0500

    finished confirmation component

--- src/components/Confirmation.js
+++ src/components/Confirmation.js
@@ -1,15 +1,14 @@
 import React from "react"
 
 
-const Confirmation = props => {
+const Confirmation = () => {
 
 
 
     return (
         <>
-
-
-        
+            <h1>Order has been Submitted!</h1>
+            <img src="https://media1.tenor.com/images/7013a56cc158a4e1c55bf364b1115388/tenor.gif?itemid=4088893" alt="dog with pizza in"></img>
         </>
     )
 }
